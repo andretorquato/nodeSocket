391d7d959c46e190719de3565dcb81fc49d545f3
André Torquato <andretorquato@alu.ufc.br>
2021-10-28 23:46:18 -0300

    config mongo

--- src/http.ts
+++ src/http.ts
@@ -2,10 +2,17 @@ import express from "express";
 import path from "path"
 import { createServer } from "http";
 import { Server } from "socket.io";
+import mongoose from "mongoose";
+
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose.connect("mongodb://localhost/nodesocket", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
