0cf7bb27eda0118475af4cb8ecf4e1d1bc7d5494
André Torquato <andretorquato@alu.ufc.br>
2021-11-01 19:07:47 -0300

    create user service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import { injectable } from "tsyringe";
+import { User } from "../schemas/User";
+interface CreateUserDTO {
+  avatar: string;
+  email: string;
+  name: string;
+  socket_id: string;
+}
+
+@injectable()
+class CreateUserService {
+  async execute({ avatar, email, name, socket_id }: CreateUserDTO) {
+    const userAlreadyExists = await User.findOne({
+      email,
+    }).exec();
+
+    if (userAlreadyExists) {
+      const user = await User.findOneAndUpdate(
+        {
+          _id: userAlreadyExists._id,
+        },
+        {
+          $set: { socket_id, avatar, name },
+        }
+      );
+      return user;
+    } else {
+      const user = await User.create({
+        avatar,
+        email,
+        name,
+        socket_id,
+      });
+      return user;
+    }
+  }
+}
+
+export { CreateUserService };
