92d4d929f202dccdfcff46c81536e996d8e36cac
André Torquato <andretorquato@alu.ufc.br>
2021-11-02 23:26:47 -0300

    create user into bd

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,7 +1,12 @@
+import { container } from "tsyringe";
 import { io } from "../http";
+import { CreateUserService } from "../services/CreateUserService";
+
 io.on('connect', socket => {
-  
-  socket.on('init_chat', socket => {
-    message: 'Chat init'
+  socket.on('start', async data => {
+    const { name, email, avatar } = data;
+    const createUserService = container.resolve(CreateUserService);
+    const user = await createUserService.execute({ name, email, avatar, socket_id: socket.id });
+    console.log(user);
   });
 });
