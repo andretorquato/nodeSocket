e70af9394daac90d6a4ef2ac8807f72e957c460f
André Torquato <andretorquato@alu.ufc.br>
2021-11-03 23:10:28 -0300

    get all users

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,39 @@ function onLoad() {
   const name = urlParams.get("name");
   const avatar = urlParams.get("avatar");
   const email = urlParams.get("email");
+  document.querySelector(".user_logged").innerHTML += `
+    <img
+      class="avatar_user_logged"
+      src="${avatar}"
+    />
+    <strong id="user_logged">${name}</strong>
+  `;
+
   socket.emit("start", { name, avatar, email });
+
+  socket.on("new_users", (data) => {
+    addUsers(data);
+  });
+
+  socket.emit("get_users", (data) => {
+    console.log(data);
+  });
 }
 
 onLoad();
+
+function addUsers(user) {
+  document.getElementById("users_list").innerHTML += `
+    <li
+      class="user_name_list"
+      id="user_${user._id}"
+      idUser="${user._id}"
+    >
+      <img
+        class="nav_avatar"
+        src="${user.avatar}"
+      />
+      ${user.name}
+    </li>
+  `;
+}
--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,12 +1,20 @@
 import { container } from "tsyringe";
 import { io } from "../http";
 import { CreateUserService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
 
 io.on('connect', socket => {
   socket.on('start', async data => {
     const { name, email, avatar } = data;
     const createUserService = container.resolve(CreateUserService);
     const user = await createUserService.execute({ name, email, avatar, socket_id: socket.id });
-    console.log(user);
+
+    socket.broadcast.emit("new_users", user);
+
+    socket.on("get_users", async (callback) => {
+      const getAllUsersService = container.resolve(GetAllUsersService);
+      const users = await getAllUsersService.execute();
+      callback(users);
+    });
   });
 });
